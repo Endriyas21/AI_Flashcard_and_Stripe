09c9b7d9818eced033a0d65acd027bd2e9da5126
Swiftminale <minalefetene@gmail.com>
2024-08-18 11:53:38 +0300

    flashcard Preview Brocken, fix next

--- app/generate/page.js
+++ app/generate/page.js
@@ -2,7 +2,23 @@
 
 import { collection, getDoc, writeBatch, doc } from "firebase/firestore";
 import { useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation"; //changed from next/router since we are using next 12 and the app directory as root
+import {
+  Container,
+  Box,
+  Typography,
+  Paper,
+  TextField,
+  Button,
+  Grid,
+  CardActionArea,
+  CardContent,
+  DialogContentText,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
 
 export default function generateKey() {
   const { isloaded, isSignedIn, user } = useState();
@@ -65,4 +81,138 @@ export default function generateKey() {
     handleClose();
     router.push("/flashcards");
   };
+
+  return (
+    <Container>
+      <Box
+        sx={{
+          mt: 4,
+          mb: 6,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4">Generate Flashcards</Typography>
+        <Paper sx={{ p: 4, width: "100%" }}>
+          <TextField
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            lable="Enter Text"
+            fullWidth
+            multiline
+            rows={4}
+            variant="outlined"
+            sx={{ mb: 2 }}
+          />
+          <Button
+            onClick={handleSubmit}
+            variant="contained"
+            color="primary"
+            fullWidth
+          >
+            Submit
+          </Button>
+        </Paper>
+      </Box>
+      {flashcards.length > 0 && (
+        <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+          <Typography variant="h5">Flashcards Preview</Typography>
+          <Grid container spacing={2}>
+            {flashcards.map((flashcard, index) => (
+              <Grid item xs={12} sm={6} md={4} key={index}>
+                <CardActionArea
+                  onClick={() => {
+                    handleCardClick(index);
+                  }}
+                >
+                  <CardContent>
+                    <Box
+                      sx={{
+                        perspective: "1000px",
+                        "& > div": {
+                          transition: "transform 0.6s",
+                          transformStyle: "preserve-3d",
+                          position: "relative",
+                          width: "100%",
+                          height: "200%",
+                          boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+                          transform: flipped[index]
+                            ? "rotateY(180deg)"
+                            : "rotateY(0deg)",
+                        },
+                        "& > div > div": {
+                          position: "absolute",
+                          width: "100%",
+                          height: "100%",
+                          backfaceVisibility: "hidden",
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                          padding: 2,
+                          boxSizing: "border-box",
+                        },
+                        "& > div > div:nth-of-type(2)": {
+                          transform: "roterY(180deg)",
+                        },
+                      }}
+                    >
+                      <div>
+                        <div>
+                          <Typography variant="h5" component="div">
+                            {flashcard.front}
+                          </Typography>
+                        </div>
+                        <div>
+                          <Typography variant="h5" component="div">
+                            {flashcard.back}
+                          </Typography>
+                        </div>
+                      </div>
+                    </Box>
+                  </CardContent>
+                </CardActionArea>
+              </Grid>
+            ))}
+          </Grid>
+          <Box sx={{ mt: 4, display: "flex", justifyContent: "center" }}>
+            <Button variant="contained" color="secondary" onClick={handleOpen}>
+              Save
+            </Button>
+          </Box>
+        </Box>
+      )}
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Save Flashcards</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please Enter a name for your flashcards collection
+          </DialogContentText>
+          <TextField
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            label="Enter a name for the flashcard set"
+            fullWidth
+            variant="outlined"
+            sx={{ mb: 2 }}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={saveFlashcards}
+            fullWidth
+          >
+            Save
+          </Button>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={saveFlashcards} color="primary">
+            {" "}
+            Save{" "}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Container>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
 import Image from "next/image";
 import getStripe from "./utils/get-stripe";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { AppBar, Container, Grid, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Container,
+  Grid,
+  Toolbar,
+  Typography,
+  Box,
+} from "@mui/material";
 import Head from "next/head";
 export default function Home() {
   return (
