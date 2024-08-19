c65d6671b9d3b8ba9cf586f0cbe5eef9b7d264a3
Swiftminale <minalefetene@gmail.com>
2024-08-19 13:22:41 +0300

    Fixed_the_get_started_route

--- app/flashcards/page.js
+++ app/flashcards/page.js
@@ -1,8 +1,28 @@
-'use client'
+"use client";
 
-import { useUser } from "@clerk/nextjs"
-import { useState, useEffect } from "react"
+import { useUser } from "@clerk/nextjs";
+import { useState, useEffect } from "react";
 
-import { CollectionReference, doc, getDoc, setDoc } from "firebase/firestore"
-import { db } from "../../firebase"
-import {useRouter} from " 'next"
+import { CollectionReference, doc, getDoc, setDoc } from "firebase/firestore";
+import { db } from "../../firebase";
+import { useRouter } from "next/navigation";
+
+export default function Flashcards() {
+  const { isLoaded, isSignedIn, user } = useUser();
+  const [flashcards, setFlashcards] = useState([]);
+
+  useEffect(() => {
+    async function getFlashcards() {
+      if (!user) return;
+      const docRef = doc(collection(db, "users"), user.id);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        const collections = docSnap.data().flashcards || [];
+        setFlashcards(collections);
+      } else {
+        await setDoc(docRef, { flashcards: [] });
+      }
+    }
+  });
+}
